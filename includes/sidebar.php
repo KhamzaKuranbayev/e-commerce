4d0e1a26f32cddbc58aae789b119e5ca96b93496
Khorezmiy1991 <khamza@inbox.ru>
2020-07-11 20:43:51 +0500

    Create e-commerce for booking shop project

---
+++ includes/sidebar.php
@@ -0,0 +1,33 @@
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            Products Categories
+        </h3>
+    </div>
+    <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked category-menu">
+            <li><a href="#">Диний-маърифий</a></li>
+            <li><a href="#">Болалар учун</a></li>
+            <li><a href="#">Бадиий адабиёт</a></li>
+            <li><a href="#">Жаҳон адабиёти</a></li>
+            <li><a href="#">Ўқув қўлланмалар</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            Categories
+        </h3>
+    </div>
+    <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked category-menu">
+            <li><a href="#">Қуръони Карим</a></li>
+            <li><a href="#">Китоблар</a></li>
+            <li><a href="#">Дисклар</a></li>
+            <li><a href="#">Нашриётлар</a></li>
+            <li><a href="#">Экспорт учун китоблар</a></li>
+        </ul>
+    </div>
+</div>
